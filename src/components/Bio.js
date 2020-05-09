6a6793aa89f49f30262a65048f1e25681de0cfec
Andrew Mok <9611545+andrewmmc@users.noreply.github.com>
2020-05-09 14:04:51 +0800

    fix build error

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
+import { graphql, useStaticQuery, Link } from 'gatsby';
 import Image from 'gatsby-image';
 import styled from 'styled-components';
 
@@ -22,8 +22,7 @@ const Container = styled.div`
 const StyledImage = styled(Image)`
   max-width: 25px;
   border-radius: 50%;
-  margin-right: 1rem;
-  margin-bottom: 0;
+  margin: 0 0.5rem 0 0;
 `;
 
 const pageQuery = graphql`
@@ -33,6 +32,11 @@ const pageQuery = graphql`
         fixed(width: 25, height: 25) {
           ...GatsbyImageSharpFixed
         }
+      }
+    }
+    site {
+      siteMetadata {
+        author
       }
     }
   }
