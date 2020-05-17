aa863fa37b17196fe1b85448ba4e964475592fbb
Andrew Mok <9611545+andrewmmc@users.noreply.github.com>
2020-05-18 02:36:43 +0800

    Fix confirmation and thanks page

--- src/pages/confirmation.js
+++ src/pages/confirmation.js
@@ -1,40 +1,34 @@
 import React from 'react';
 import { shape } from 'prop-types';
-import { graphql, Link } from 'gatsby';
-import styled from 'styled-components';
+import { graphql } from 'gatsby';
 
-import Layout from 'components/Layout';
-import Seo from 'components/Seo';
 import Thumbnail from 'components/Thumbnail';
+import BasePage from 'templates/basePage';
 
 const Confirmation = ({ data }) => (
-  <Layout
-    cover={<Thumbnail fluid={data.featuredImage.childImageSharp.fluid} />}
+  <BasePage
+    title="One Last Step..."
+    thumbnail={<Thumbnail fluid={data.featuredImage.childImageSharp.fluid} />}
   >
-    <Seo title="One Last Step" />
-    <Main>
-      <h1>One Last Step...</h1>
-      <p>
-        Thanks for signing up. Please check your inbox and{' '}
-        <strong>confirm your subscription</strong>.
-      </p>
-    </Main>
-  </Layout>
+    <p>
+      Thanks for signing up. Please check your inbox and{' '}
+      <strong>confirm your subscription</strong>.
+    </p>
+  </BasePage>
 );
 
 Confirmation.propTypes = {
   data: shape({}).isRequired,
 };
 
-const Main = styled.div`
-  margin: 2rem 0;
-`;
-
 export default Confirmation;
 
 export const pageQuery = graphql`
   query {
-    featuredImage: file(relativePath: { eq: "assets/confirm.jpg" }) {
+    featuredImage: file(
+      sourceInstanceName: { eq: "assets" }
+      relativePath: { eq: "confirm.jpg" }
+    ) {
       childImageSharp {
         fluid(quality: 90, maxWidth: 1440) {
           ...GatsbyImageSharpFluid_withWebp
--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import { shape } from 'prop-types';
 import { graphql, Link } from 'gatsby';
-import styled from 'styled-components';
 
-import Layout from 'components/Layout';
-import Seo from 'components/Seo';
 import Thumbnail from 'components/Thumbnail';
+import BasePage from 'templates/basePage';
 
 const Thanks = ({ data }) => (
-  <Layout
-    cover={<Thumbnail fluid={data.featuredImage.childImageSharp.fluid} />}
+  <BasePage
+    title="Thank you for subscribing"
+    thumbnail={<Thumbnail fluid={data.featuredImage.childImageSharp.fluid} />}
   >
-    <Seo title="Thank you for subscribing" />
-    <Main>
-      <h1>Thank you for subscribing</h1>
+    <>
       <p>
         You are now confirmed for subscription. You will receive emails when I
         post new content.
@@ -22,23 +19,22 @@ const Thanks = ({ data }) => (
         <strong>No spam</strong> - You can unsubscribe at <i>any time</i>.
       </p>
       <Link to="/">Back to home</Link>
-    </Main>
-  </Layout>
+    </>
+  </BasePage>
 );
 
 Thanks.propTypes = {
   data: shape({}).isRequired,
 };
 
-const Main = styled.div`
-  margin: 2rem 0;
-`;
-
 export default Thanks;
 
 export const pageQuery = graphql`
   query {
-    featuredImage: file(relativePath: { eq: "assets/thanks.jpg" }) {
+    featuredImage: file(
+      sourceInstanceName: { eq: "assets" }
+      relativePath: { eq: "thanks.jpg" }
+    ) {
       childImageSharp {
         fluid(quality: 90, maxWidth: 1440) {
           ...GatsbyImageSharpFluid_withWebp
